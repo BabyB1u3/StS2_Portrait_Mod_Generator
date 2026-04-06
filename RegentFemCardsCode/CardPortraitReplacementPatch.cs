6686ea35e3b44beb2a0c7cd02360c0a9f381f067
Rigel <112126663+BabyB1u3@users.noreply.github.com>
2026-04-06 14:43:40 -0400

    Clean up portrait getter

--- RegentFemCardsCode/CardPortraitReplacementPatch.cs
+++ RegentFemCardsCode/CardPortraitReplacementPatch.cs
@@ -1,109 +1,12 @@
-using Godot;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Models;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
 
 namespace RegentFemCards.RegentFemCardsCode;
 
-[HarmonyPatch]
+[HarmonyPatch(typeof(CardModel), "PortraitPath", MethodType.Getter)]
 internal static class CardPortraitReplacementPatch
 {
-    private static readonly BindingFlags InstanceBindings =
-        BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
-
-    private static readonly string[] PreferredPropertyNames =
-    [
-        "PortraitPath",
-        "Portrait",
-        "CardArtPath",
-        "CardArt"
-    ];
-
-    private static readonly string[] PreferredMethodNames =
-    [
-        "GetPortraitPath",
-        "GetPortrait",
-        "GetCardArtPath",
-        "GetCardArt"
-    ];
-
-    private static bool _loggedTargets;
-
-    private static IEnumerable<MethodBase> TargetMethods()
-    {
-        HashSet<MethodBase> targets = [];
-
-        AddTargetsFromType(targets, typeof(CardModel));
-
-        foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
-        {
-            Type[] types;
-            try
-            {
-                types = assembly.GetTypes();
-            }
-            catch (ReflectionTypeLoadException ex)
-            {
-                types = ex.Types.Where(type => type is not null).Cast<Type>().ToArray();
-            }
-            catch
-            {
-                continue;
-            }
-
-            foreach (Type type in types)
-            {
-                if (!typeof(CardModel).IsAssignableFrom(type) || type == typeof(CardModel))
-                {
-                    continue;
-                }
-
-                AddTargetsFromType(targets, type);
-            }
-        }
-
-        if (!_loggedTargets)
-        {
-            _loggedTargets = true;
-            if (targets.Count == 0)
-            {
-                GD.PushWarning("[RegentFemCards] No explicit portrait getter target was found. Portrait replacement patch will be inactive.");
-            }
-            else
-            {
-                foreach (MethodBase target in targets.OrderBy(static method => method.DeclaringType?.FullName).ThenBy(static method => method.Name))
-                {
-                    GD.Print($"[RegentFemCards] Patching portrait getter: {target.DeclaringType?.FullName}.{target.Name}");
-                }
-            }
-        }
-
-        return targets;
-    }
-
-    private static void AddTargetsFromType(ISet<MethodBase> targets, Type type)
-    {
-        foreach (string propertyName in PreferredPropertyNames)
-        {
-            PropertyInfo? property = type.GetProperty(propertyName, InstanceBindings);
-            if (property?.PropertyType == typeof(string) && property.GetMethod is not null)
-            {
-                targets.Add(property.GetMethod);
-            }
-        }
-
-        foreach (string methodName in PreferredMethodNames)
-        {
-            MethodInfo? method = type.GetMethod(methodName, InstanceBindings, null, Type.EmptyTypes, null);
-            if (method?.ReturnType == typeof(string))
-            {
-                targets.Add(method);
-            }
-        }
-    }
-
     [SuppressMessage("Style", "IDE0051", Justification = "Harmony discovers patch methods via reflection.")]
     [SuppressMessage("Style", "IDE1006", Justification = "Harmony requires the __instance parameter name.")]
     [HarmonyPostfix]
